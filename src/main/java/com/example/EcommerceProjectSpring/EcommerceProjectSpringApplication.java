f4cd8d4b75b9422b57dcc1673d5c3f746da3179f
Naman <goelnaman66@gmail.com>
2025-07-12 01:21:07 +0530

    load .env file

--- src/main/java/com/example/EcommerceProjectSpring/EcommerceProjectSpringApplication.java
+++ src/main/java/com/example/EcommerceProjectSpring/EcommerceProjectSpringApplication.java
@@ -1,12 +1,20 @@
 package com.example.EcommerceProjectSpring;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class EcommerceProjectSpringApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load(); // load .env file from the root path
+
+		dotenv.entries().forEach( (DotenvEntry entry) -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(EcommerceProjectSpringApplication.class, args);
 	}
 
