7faa0465885431f75c51bfd07a7c9d54b00de81d
Naman <goelnaman66@gmail.com>
2025-07-11 23:20:44 +0530

    initial commit

---
+++ src/main/java/com/example/EcommerceProjectSpring/EcommerceProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcommerceProjectSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceProjectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceProjectSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EcommerceProjectSpring/EcommerceProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcommerceProjectSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProjectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
